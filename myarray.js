b32895e33a3ba62867325336b2b4d03a7bf7ea12
unknown <john5cele@yahoo.com>
2025-02-06 11:03:13 +0100

    arr last

--- myarray.js
+++ myarray.js
@@ -6,3 +6,6 @@ let array5 = new Array(6);
 //get lenght
 let arr = [1,2,2,4];
 arr.length;
+//Get the first item, the middle item and the last item of the array
+arr[0];//gets the first
+arr[arr.length];// gets the last element
